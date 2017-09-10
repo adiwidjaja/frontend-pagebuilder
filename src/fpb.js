f7d1f0935429cf8d1faa109dc0e43a357cc6f7bd
Andy Adiwidjaja <adiwidjaja@gmail.com>
2017-09-10 12:09:06 +0200

    Use react.

--- src/fpb.js
+++ src/fpb.js
@@ -20,63 +20,63 @@ var forEach = function (array, callback, scope) {
 class FrontendPageBuilder {
     constructor() {
     }
-    init() {
-        tinymce.init({
-            selector: '[data-sfb-valuetype="string"]',
-            inline: true,
-            toolbar: 'undo redo',
-            menubar: false
-        });
+    init(selector) {
+        // tinymce.init({
+        //     selector: '[data-sfb-valuetype="string"]',
+        //     inline: true,
+        //     toolbar: 'undo redo',
+        //     menubar: false
+        // });
 
-        tinymce.init({
-            selector: '[data-sfb-valuetype="rich"]',
-            inline: true,
-            // toolbar: 'undo redo',
-            menubar: false
-        });
+        // tinymce.init({
+        //     selector: '[data-sfb-valuetype="rich"]',
+        //     inline: true,
+        //     // toolbar: 'undo redo',
+        //     menubar: false
+        // });
 
-        function serializeContent() {
-            var areas = document.querySelectorAll("[data-sfb-content]");
-            forEach(areas, function(i, area){
-                var data = [];
-                var sections = area.querySelectorAll("[data-sfb-section]");
-                forEach(sections, function(i, section){
-                    var sectiondata = {};
-                    var fields = section.querySelectorAll("[data-sfb-value]");
-                    forEach(fields, function(i, field) {
-                        var name = field.getAttribute("data-sfb-value");
-                        var type = field.getAttribute("data-sfb-valuetype");
-                        var value = field.innerHTML;
-                        sectiondata[name] = value;
-                    });
-                    data.push(sectiondata);
-                });
-                console.log(JSON.stringify(data));
-            });
-        }
+        // function serializeContent() {
+        //     var areas = document.querySelectorAll("[data-sfb-content]");
+        //     forEach(areas, function(i, area){
+        //         var data = [];
+        //         var sections = area.querySelectorAll("[data-sfb-section]");
+        //         forEach(sections, function(i, section){
+        //             var sectiondata = {};
+        //             var fields = section.querySelectorAll("[data-sfb-value]");
+        //             forEach(fields, function(i, field) {
+        //                 var name = field.getAttribute("data-sfb-value");
+        //                 var type = field.getAttribute("data-sfb-valuetype");
+        //                 var value = field.innerHTML;
+        //                 sectiondata[name] = value;
+        //             });
+        //             data.push(sectiondata);
+        //         });
+        //         console.log(JSON.stringify(data));
+        //     });
+        // }
 
-        var buttonrow = '<ul class="sfb-tools"><li class="sfb-tools_handle"></li><li class="sfb-tools_edit"></li><li class="sfb-tools_trash"></li><li class="sfb-tools_show"></li></ul><div class="sfb-overlay"><span></span><span></span><span></span><span></span></div>'
+        // var buttonrow = '<ul class="sfb-tools"><li class="sfb-tools_handle"></li><li class="sfb-tools_edit"></li><li class="sfb-tools_trash"></li><li class="sfb-tools_show"></li></ul><div class="sfb-overlay"><span></span><span></span><span></span><span></span></div>'
 
-        ready(function() {
+        // ready(function() {
 
-            var areas = document.querySelectorAll("[data-sfb-content]");
-            forEach(areas, function(i, area){
-                Sortable.create(area, {
-                    animation: 150,
-                    // draggable: "[data-sfb-section]",
-                    handle: ".sfb-tools_handle"
-                });
-                var sections = area.querySelectorAll("[data-sfb-section]");
-                forEach(sections, function(i, section){
-                    section.insertAdjacentHTML('afterbegin', buttonrow);
-                    // section.classList.add("sfb-draggable");
-                });
-            });
+        //     var areas = document.querySelectorAll("[data-sfb-content]");
+        //     forEach(areas, function(i, area){
+        //         Sortable.create(area, {
+        //             animation: 150,
+        //             // draggable: "[data-sfb-section]",
+        //             handle: ".sfb-tools_handle"
+        //         });
+        //         var sections = area.querySelectorAll("[data-sfb-section]");
+        //         forEach(sections, function(i, section){
+        //             section.insertAdjacentHTML('afterbegin', buttonrow);
+        //             // section.classList.add("sfb-draggable");
+        //         });
+        //     });
 
-            document.getElementById("sfb-save").addEventListener("click", function(e) {
-                serializeContent();
-            });
-        });
+        //     document.getElementById("sfb-save").addEventListener("click", function(e) {
+        //         serializeContent();
+        //     });
+        // });
     }
 }
 
